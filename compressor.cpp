af0e6ef7506496c0c02a73562a6ffec3caa676a6
Tejas007bond <tejasphalke762@gmail.com>
2026-05-25 12:57:26 +0530

    Added testing and fixed a bug : changed typecasting to int to unsigned char

--- compressor.cpp
+++ compressor.cpp
@@ -12,8 +12,24 @@ int main(void){
 
     char ch;
     int frequency[256] = {0};
-    
+
     while(file.get(ch)){
-        frequency[(int)ch]++;
+        frequency[(unsigned char)ch]++;
     }
+
+    file.close();
+
+    for(int i=0;i<256;i++){
+        if(frequency[i] > 0){
+            if (i > 32 && i < 127) {
+                cout << "'" << (char)i << "' : " << frequency[i] << endl;
+        } 
+        
+        else {
+                cout << "ASCII " << i << " : " << frequency[i] << endl;
+        }
+        }
+    }
+
+    return 0;
 }
