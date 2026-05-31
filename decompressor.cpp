a221ac1af5dbd944a5289749f3282c6bf8c03301
Tejas007bond <tejasphalke762@gmail.com>
2026-05-31 18:07:15 +0530

    Successfully readed the metadata of the file

--- decompressor.cpp
+++ decompressor.cpp
@@ -7,3 +7,16 @@
 
 using namespace std;
 
+int main(){
+    ifstream inFile("compressed.bin",ios::binary);
+    ofstream outFile("output_restored.txt");
+
+    if(!inFile || !outFile){
+        cout << "Error opening files for decompression" << endl;
+        return 1;
+    }
+    
+    int frequency[256] = {0};
+    inFile.read(reinterpret_cast<char*>(frequency),256*sizeof(int));
+    
+}
